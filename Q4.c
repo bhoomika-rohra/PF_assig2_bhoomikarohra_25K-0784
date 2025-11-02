2e3202b6a7572f1098fe7cdbb1fe78c4635ce00b
bhoomika-rohra <K250784@nu.edu.pk>
2025-11-02 20:51:09 +0500

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int stack[100];
-int top = -1;
-int maxSize;
-
-void pushStack();
-void popStack();
-void peekStack();
-void displayStack();
+void pushStack(int stack[], int *top, int maxSize);
+void popStack(int stack[], int *top);
+void peekStack(int stack[], int top);
+void displayStack(int stack[], int top);
 
 int main() {
+    int maxSize;
     printf("Enter the maximum size of the stack: ");
     scanf("%d", &maxSize);
 
+    int stack[maxSize];
+    int top = -1;
     int choice = 0;
+
     while(choice != 5) {
         printf("\n--- Stack Operations Menu ---\n");
         printf("1. PUSH\n");
@@ -24,52 +24,53 @@ int main() {
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        if(choice == 1){ 
-            pushStack();
-        }    
-        else if(choice == 2){
-            popStack();
-        }    
-        else if(choice == 3){ 
-            peekStack();
-        }    
-        else if(choice == 4){ 
-            displayStack();
-        }    
-        else if(choice == 5){ 
-            printf("Exiting...\n");
-        }    
-        else{ 
-            printf("Invalid choice!\n");
-        }    
+        switch(choice) {
+            case 1:
+                pushStack(stack, &top, maxSize);
+                break;
+            case 2:
+                popStack(stack, &top);
+                break;
+            case 3:
+                peekStack(stack, top);
+                break;
+            case 4:
+                displayStack(stack, top);
+                break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice!\n");
+        }
     }
 
     return 0;
 }
 
-void pushStack() {
-    if(top >= maxSize - 1) {
+void pushStack(int stack[], int *top, int maxSize) {
+    if(*top >= maxSize - 1) {
         printf("Stack Overflow! Cannot push element.\n");
         return;
     }
     int element;
     printf("Enter element to push: ");
     scanf("%d", &element);
-    top++;
-    stack[top] = element;
+    (*top)++;
+    stack[*top] = element;
     printf("%d pushed to stack.\n", element);
 }
 
-void popStack() {
-    if(top == -1) {
+void popStack(int stack[], int *top) {
+    if(*top == -1) {
         printf("Stack Underflow! Cannot pop element.\n");
         return;
     }
-    printf("%d popped from stack.\n", stack[top]);
-    top--;
+    printf("%d popped from stack.\n", stack[*top]);
+    (*top)--;
 }
 
-void peekStack() {
+void peekStack(int stack[], int top) {
     if(top == -1) {
         printf("Stack is empty. Nothing to peek.\n");
         return;
@@ -77,7 +78,7 @@ void peekStack() {
     printf("Top element: %d\n", stack[top]);
 }
 
-void displayStack() {
+void displayStack(int stack[], int top) {
     if(top == -1) {
         printf("Stack is empty.\n");
         return;
@@ -88,3 +89,4 @@ void displayStack() {
     }
     printf("\n");
 }
+
