12274f4f250722806e37f47a7abded1a98267697
bhoomika-rohra <K250784@nu.edu.pk>
2025-11-02 20:47:02 +0500

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 
-void getcustomerinfo();
-void displayinventory();
-void updateinventory(int index, int q);
-void additem();
-float displaybill();
-void showinvoice();
-
-int productCodes[4] = {1, 2, 3, 4};
-int quantities[4] = {50, 10, 20, 8};
-float prices[4] = {100, 200, 300, 150};
-char name[20];
-char cnic[30];
-int cartitem[100];
-int qty[100];
-int cartcount = 0;
+void getcustomerinfo(char name[], char cnic[]);
+void displayinventory(int productCodes[], int quantities[], float prices[], int size);
+void updateinventory(int quantities[], int index, int q);
+void additem(int productCodes[], int quantities[], float prices[], int cartitem[], int qty[], int *cartcount, int size);
+float displaybill(int productCodes[], float prices[], int cartitem[], int qty[], int cartcount);
+void showinvoice(char name[], char cnic[], int productCodes[], float prices[], int cartitem[], int qty[], int cartcount);
 
 int main() {
+    int productCodes[4] = {1, 2, 3, 4};
+    int quantities[4] = {50, 10, 20, 8};
+    float prices[4] = {100, 200, 300, 150};
+    char name[20], cnic[30];
+    int cartitem[100], qty[100], cartcount = 0;
     int choice = 0;
+    int size = 4;
+
     while (choice != 7) {
         printf("\n--- Supermarket Menu ---\n");
         printf("1. Customer Information\n");
@@ -30,48 +28,55 @@ int main() {
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        if (choice == 1) {
-            getcustomerinfo();
-        } else if (choice == 2) {
-            displayinventory();
-        } else if (choice == 3) {
-            additem();
-        } else if (choice == 4) {
-            displaybill();
-        } else if (choice == 5) {
-            showinvoice();
-        } else if (choice == 7) {
-            printf("Exiting.\n");
-        } else {
-            printf("Invalid choice!\n");
+        switch(choice) {
+            case 1:
+                getcustomerinfo(name, cnic);
+                break;
+            case 2:
+                displayinventory(productCodes, quantities, prices, size);
+                break;
+            case 3:
+                additem(productCodes, quantities, prices, cartitem, qty, &cartcount, size);
+                break;
+            case 4:
+                displaybill(productCodes, prices, cartitem, qty, cartcount);
+                break;
+            case 5:
+                showinvoice(name, cnic, productCodes, prices, cartitem, qty, cartcount);
+                break;
+            case 7:
+                printf("Exiting.\n");
+                break;
+            default:
+                printf("Invalid choice!\n");
         }
     }
     return 0;
 }
 
-void getcustomerinfo() {
+void getcustomerinfo(char name[], char cnic[]) {
     printf("Enter customer name: ");
     scanf(" %[^\n]", name);
     printf("Enter CNIC number: ");
     scanf(" %[^\n]", cnic);
 }
 
-void displayinventory() {
+void displayinventory(int productCodes[], int quantities[], float prices[], int size) {
     printf("\nCode\tQuantity\tPrice\n");
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < size; i++) {
         printf("%d\t%d\t\t%.2f\n", productCodes[i], quantities[i], prices[i]);
     }
 }
 
-void updateinventory(int index, int q) {
+void updateinventory(int quantities[], int index, int q) {
     quantities[index] -= q;
 }
 
-void additem() {
+void additem(int productCodes[], int quantities[], float prices[], int cartitem[], int qty[], int *cartcount, int size) {
     int code, q, found = 0;
     printf("Enter product code to add to cart: ");
     scanf("%d", &code);
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < size; i++) {
         if (productCodes[i] == code) {
             found = 1;
             printf("Enter quantity: ");
@@ -80,10 +85,10 @@ void additem() {
                 printf("Insufficient stock!\n");
                 return;
             }
-            cartitem[cartcount] = i;
-            qty[cartcount] = q;
-            cartcount++;
-            updateinventory(i, q);
+            cartitem[*cartcount] = i;
+            qty[*cartcount] = q;
+            (*cartcount)++;
+            updateinventory(quantities, i, q);
             printf("%d item(s) of code %d added to cart!\n", q, code);
             return;
         }
@@ -93,9 +98,10 @@ void additem() {
     }
 }
 
-float displaybill() {
+float displaybill(int productCodes[], float prices[], int cartitem[], int qty[], int cartcount) {
     float total = 0;
     char promocode[20];
+
     printf("\nCode\tQuantity\tPrice\tSubtotal\n");
     for (int i = 0; i < cartcount; i++) {
         float subtotal = prices[cartitem[i]] * qty[i];
@@ -106,19 +112,6 @@ float displaybill() {
 
     printf("Enter promocode (if any): ");
     scanf(" %[^\n]", promocode);
-    if (promocode[0] == 'E' && promocode[1] == 'i' && promocode[2] == 'd' &&
-        promocode[3] == '2' && promocode[4] == '0' && promocode[5] == '2' &&
-        promocode[6] == '5' && promocode[7] == '\0') {
-        total *= 0.75;
-        printf("25%% discount applied!\n");
-    }
 
-    printf("Final Bill: %.2f\n", total);
-    return total;
-}
-
-void showinvoice() {
-    printf("\n--- Invoice ---\n");
-    printf("Name: %s\nCNIC: %s\n", name, cnic);
-    displaybill();
-}
+    // Manual comparison with "Eid2025"
+    if (promocode[0]()
